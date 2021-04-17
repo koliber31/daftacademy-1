662edf051294e19929a51e320b3a889e9b8c7109
Lukasz Szymanski <Luk_szym@wp.pl>
2021-04-18 00:09:20 +0200

    task 1.2

--- main.py
+++ main.py
@@ -6,3 +6,33 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": 'Hello world!'}
+
+
+@app.get("/hello/{name}")
+def hello_name_view(name: str):
+    return f"Hello {name}"
+
+
+@app.get("/method")
+def method_id():
+    return {"method": 'GET'}
+
+
+@app.post("/method", status_code=201)
+def method_id():
+    return {"method": 'POST'}
+
+
+@app.delete("/method")
+def method_id():
+    return {"method": 'DELETE'}
+
+
+@app.put("/method")
+def method_id():
+    return {"method": 'PUT'}
+
+
+@app.options("/method")
+def method_id():
+    return {"method": 'OPTIONS'}
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
-from fastapi.testclient import TestClient
+import pytest
 
+from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
@@ -9,3 +10,40 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'message': 'Hello world!'}
+
+
+@pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
+def test_hello_name(name):
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.text == f'"Hello {name}"'
+
+
+def test_method_code_get():
+    response = client.get("/method")
+    assert response.status_code == 200
+    assert response.json() == {'method': 'GET'}
+
+
+def test_method_code_put():
+    response = client.put('/method')
+    assert response.status_code == 200
+    assert response.json() == {'method': 'PUT'}
+
+
+def test_method_code_delete():
+    response = client.delete('/method')
+    assert response.status_code == 200
+    assert response.json() == {'method': 'DELETE'}
+
+
+def test_method_code_options():
+    response = client.options('/method')
+    assert response.status_code == 200
+    assert response.json() == {'method': 'OPTIONS'}
+
+
+def test_method_code_post():
+    response = client.post("/method")
+    assert response.status_code == 201
+    assert response.json() == {'method': 'POST'}
