608d434f3e81b779e9c77fbfdde4776549676c3e
Lukasz Szymanski <Luk_szym@wp.pl>
2021-05-05 21:57:51 +0200

    single supplier view added

--- tests.py
+++ tests.py
@@ -1,34 +1,3 @@
-import pytest
-
 from fastapi.testclient import TestClient
 from main import app
 
-client = TestClient(app)
-
-
-def test_hello_view():
-    response = client.get("/hello")
-    assert response.status_code == 200
-
-
-def test_login_session_ok():
-    response = client.post('/login_session?user=4dm1n&password=NotSoSecurePa$$')
-    assert response.status_code == 200
-    # assert response.set_cookie ==
-
-
-def test_login_session_nok():
-    response = client.post('/login_session?user=4dm1n&password=wrong')
-    assert response.status_code == 401
-
-
-def test_login_token_ok():
-    response = client.post('/login_token?user=4dm1n&password=NotSoSecurePa$$')
-    assert response.status_code == 200
-    assert response.json() == {"token": 'c8730304beb5f0e94434af0869469a86069a2aa7a824e95b7e8cee9ba14bbac6'}
-
-
-def test_foo():
-    response = client.post('/foo')
-    assert response.status_code == 200
-    assert response.json() == {'foo': 'bar'}
