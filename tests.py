af8de5e7c6e9b3a37a87c0105541c1c70e5634ac
Lukasz Szymanski <Luk_szym@wp.pl>
2021-05-05 23:05:53 +0200

    northwind.db added

--- tests.py
+++ tests.py
@@ -1,3 +1,14 @@
 from fastapi.testclient import TestClient
 from main import app
 
+client = TestClient(app)
+
+
+def test_categories():
+    response = client.get("/categories")
+    assert response.status_code == 200
+
+
+def test_customers():
+    response = client.get("/customers")
+    assert response.status_code == 200
