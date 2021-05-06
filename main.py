e0d47fe6cc21c497dcadb08a88e1534c442e508b
Lukasz Szymanski <Luk_szym@wp.pl>
2021-05-06 16:25:16 +0200

    Task 4.4

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
-    app.db_connection = sqlite3.connect("northwind.db")
+    app.db_connection = sqlite3.connect("northwind.db", check_same_thread=False)
     app.db_connection.text_factory = lambda b: b.decode(errors="ignore")  # northwind specific
 
 
@@ -62,7 +62,7 @@ async def products(product_id: int):
 
 
 @app.get("/employees/")
-async def products(limit: Optional[int] = None, offset: Optional[int] = None, order='id'):
+async def employees(limit: Optional[int] = None, offset: Optional[int] = None, order='id'):
     app.db_connection.row_factory = sqlite3.Row
     if order not in ['id', 'first_name', 'last_name', 'city']:
         raise HTTPException(status_code=400, detail='Wrong order parameter')
@@ -74,3 +74,16 @@ async def products(limit: Optional[int] = None, offset: Optional[int] = None, or
     data = app.db_connection.execute(
         f"SELECT EmployeeId AS id, LastName AS last_name, FirstName AS first_name, City AS city FROM employees ORDER BY {order}" + limitation).fetchall()
     return {'employees': data}
+
+
+@app.get("/products_extended")
+def products_extended():
+    app.db_connection.row_factory = sqlite3.Row
+    data = app.db_connection.execute('''
+                                     SELECT products.ProductID AS id, products.ProductName AS name,
+                                     categories.CategoryName AS category, suppliers.CompanyName AS supplier
+                                     FROM products JOIN categories ON products.CategoryId = categories.CategoryId
+                                     JOIN suppliers ON products.SupplierId = suppliers.SupplierId
+                                     ''').fetchall()
+
+    return {'products_extended': data}
